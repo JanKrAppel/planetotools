a9223c1453312db8228c098903b9e4cd0559c87a
Jan Kristoffer Appel <appel@physik.uni-kiel.de>
2016-06-23 15:15:45 +0200

    Made plot_data_overview not plot any titles for the subplots

--- mcdtools.py
+++ mcdtools.py
@@ -8,18 +8,19 @@ from planetotools import __check_xy_units
 if not plt.isinteractive():
     plt.ion()
 
-def plot_temp_profile(mcddata, *args, **kwargs):
+def plot_temp_profile(mcddata, title=True, *args, **kwargs):
     """Plot a temperature profile for the given MCD data object."""
     plt.plot(mcddata.data['temp'], mcddata.data['xz'], *args, **kwargs)
     __check_xy_units('K', 'km')
     plt.xlabel('Temperature / K')
     plt.ylabel('Height / km')
     plt.ylim(mcddata.params['surface_height'], mcddata.params['max_height'])
-    plt.title('Temperature profile, ' + mcddata.get_coords_scenario_str())
+    if title:
+        plt.title('Temperature profile, ' + mcddata.get_coords_scenario_str())
     plt.show(block = False)
     return
     
-def plot_pres_profile(mcddata, *args, **kwargs):
+def plot_pres_profile(mcddata, title=True, *args, **kwargs):
     """Plot a pressure profile for the given MCD data object."""
     plt.plot(mcddata.data['pres'], mcddata.data['xz'], *args, **kwargs)
     __check_xy_units('Pa', 'km')
@@ -27,11 +28,12 @@ def plot_pres_profile(mcddata, *args, **kwargs):
     plt.ylabel('Height / km')
     plt.xscale('log')
     plt.ylim(mcddata.params['surface_height'], mcddata.params['max_height'])
-    plt.title('Pressure profile, ' + mcddata.get_coords_scenario_str())
+    if title:
+        plt.title('Pressure profile, ' + mcddata.get_coords_scenario_str())
     plt.show(block = False)
     return
         
-def plot_dens_profile(mcddata, *args, **kwargs):
+def plot_dens_profile(mcddata, title=True, *args, **kwargs):
     """Plot a density profile for the given MCD data object."""
     plt.plot(mcddata.data['dens'], mcddata.data['xz'], *args, **kwargs)
     __check_xy_units('g/cm3', 'km')
@@ -39,11 +41,12 @@ def plot_dens_profile(mcddata, *args, **kwargs):
     plt.ylabel('Height / km')
     plt.xscale('log')
     plt.ylim(mcddata.params['surface_height'], mcddata.params['max_height'])
-    plt.title('Density profile, ' + mcddata.get_coords_scenario_str())
+    if title:
+        plt.title('Density profile, ' + mcddata.get_coords_scenario_str())
     plt.show(block = False)
     return
 
-def plot_comp_profile(mcddata, plot_only = None, dont_plot = None, *args, **kwargs):
+def plot_comp_profile(mcddata, plot_only = None, dont_plot = None, title=True, *args, **kwargs):
     """Plot a composition profile for the given MCD data object."""
     if 'label' in kwargs:
         kwargs.pop('label')
@@ -63,18 +66,20 @@ def plot_comp_profile(mcddata, plot_only = None, dont_plot = None, *args, **kwar
     plt.ylabel('Height / km')
     plt.xscale('log')
     plt.ylim(mcddata.params['surface_height'], mcddata.params['max_height'])
-    plt.title('Composition profile, ' + mcddata.get_coords_scenario_str())
+    if title:
+        plt.title('Composition profile, ' + mcddata.get_coords_scenario_str())
     plt.show(block = False)
     return
     
-def plot_shield_depth_profile(mcddata, *args, **kwargs):
+def plot_shield_depth_profile(mcddata, title=True, *args, **kwargs):
     """Plot a shielding depth profile for the given MCD data object."""
     plt.plot(mcddata.data['shield_depth'], mcddata.data['xz'], *args, **kwargs)
     plt.xlabel('Shielding depth / g/cm2')
     plt.ylabel('Height / km')
     plt.xscale('log')
     plt.ylim(mcddata.params['surface_height'], mcddata.params['max_height'])
-    plt.title('Shielding depth profile, ' + mcddata.get_coords_scenario_str())
+    if title:
+        plt.title('Shielding depth profile, ' + mcddata.get_coords_scenario_str())
     plt.show(block = False)
     return
     
@@ -82,13 +87,13 @@ def plot_data_overview(mcddata, title = False, legend = False, *args, **kwargs):
     """Plot a data overview for the given MCD data object.
     This creates a subplot with temperature, pressure, density and composition profiles."""
     plt.subplot(221)
-    plot_temp_profile(mcddata, *args, **kwargs)
+    plot_temp_profile(mcddata, title=False, *args, **kwargs)
     plt.subplot(222)
-    plot_pres_profile(mcddata, *args, **kwargs)
+    plot_pres_profile(mcddata, title=False, *args, **kwargs)
     plt.subplot(223)
-    plot_dens_profile(mcddata, *args, **kwargs)
+    plot_dens_profile(mcddata, title=False, *args, **kwargs)
     plt.subplot(224)
-    plot_shield_depth_profile(mcddata, *args, **kwargs)
+    plot_shield_depth_profile(mcddata, title=False, *args, **kwargs)
     if title:
         plt.suptitle('Data overview for ' + mcddata.params['filename'])
     if legend:
